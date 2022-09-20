f36dd381aead7b49baeb5bb20db7845e17b5ac66
jhih_yu <74854953+jhihyulin@users.noreply.github.com>
2022-09-21 03:45:32 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # ShortURL
-A simple Short URL service deploy on deta.sh
+A simple Short URL service deploy on deta.sh with FastAPI
+
+This is a simple short url service, you can use it to shorten your long url.
+
+## Deploy
+
+step 1: fork this repositorities
+
+step 2: visit https://web.deta.sh and login
+
+step 3: create a new project
+
+step 4: visit https://web.deta.sh/settings and create a new Access Token
+
+step 5: go to your repositorie's setting, add a new secret named ```DETA_PROJECT_NAME``` and set the value to the name you created in step 3
+
+step 6: go to your repositorie's setting, add a new secret named ```DETA_TOKEN``` and set the value to the token you created in step 4
+
+step 7: go to your repositorie's action page click ```Run workflow``` to deploy
+
+step 8: because of we can't use github action to update ```.env``` file now, so we have two ways to set the ```SERVER_PREFIX``` parameter, you can use your custom domain or use the deta provided domain, you can find the default domain on https://web.deta.sh micros>ShortURL-main that visiable on the righttop of the page, about hoe to setup custom domain, you can find it on https://docs.deta.sh/docs/micros/custom_domains
+
+1. go to your repositorie, path to ```micros/ShortURL-main/main.py``` and edit the ```SERVER_PREFIX``` parameter inline 20, then go to your repositorie's action page click ```Run workflow``` to redeploy
+```python
+#original
+SERVER_PREFIX = os.getenv('SERVER_PREFIX')
+#change to
+SERVER_PREFIX = "<your server prefix>"
+```
+
+
+2. clone repositorities to your local, edit the ```SERVER_PREFIX``` parameter in ```micros/ShortURL-main/.env``` and use deta cli to update env
+```env
+SERVER_PREFIX=<your server prefix>
+```
+
+### optional
+if you want develop on your local, you have some steps to do
+
+step 1: install python3.9 or higher
+
+step 2: install deta cli, you can find it on https://docs.deta.sh/docs/cli/install
+
+step 3: clone repositorie to your local
+
+step 4: ```cd micros/ShortURL-main```
+
+step 5: ```pip install -r requirements.txt```
+
+step 6: ```deta login``` to login
+
+step 7: add the ```SERVER_PREFIX``` and ```DETA_TOKEN``` parameter to ```micros/ShortURL-main/.env```
+
+```env
+SERVER_PREFIX=<your server prefix>
+DETA_TOKEN=<your deta token>
+```
+
+step 8: ```deta update -e .env``` to update env
+
+step 9: ```deta deploy``` to deploy
+
+## Usage
+```POST /create``` to create a short url
+```json
+{
+    "origin_url": "<your long url>"
+}
+```
+
+response
+```json
+{
+    "url": "<your short url>"
+}
+```
+
+```GET /<short url>``` to redirect to the long url 
+
+## Reference
+[Deploy FastAPI on Deta](https://fastapi.tiangolo.com/deployment/deta/)
+
+[Short URL API Server 架設心得與教學](https://hackmd.io/@Xpz2MX78SomsO4mV3ejdqg/SyvmmBCfX?type=view)
+
+[BogDAAAMN/deta-deploy-action](https://github.com/BogDAAAMN/deta-deploy-action)
+
---
+++ micros/ShortURL-main/main.py
@@ -0,0 +1,114 @@
+# db
+from deta import Deta
+# webserver
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+# for make short url
+import hashlib
+import time
+from urllib.parse import urlunparse, urlparse
+from lib.base62 import Base62
+# env
+import os
+from dotenv import load_dotenv
+
+# env variable
+load_dotenv()
+PROJECTING_KEY = os.getenv('DETA_PROJECT_KEY')
+SERVER_PREFIX = os.getenv('SERVER_PREFIX')
+
+# init database
+deta = Deta(PROJECTING_KEY)
+DB = deta.Base("db")
+# init webserver
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def short(url):
+    # MD5 hash and take first 40bit encdoe with Base62
+    return Base62.encodebytes(hashlib.md5(url.encode('utf-8')).digest()[-5:])
+
+
+def use_rand_key(url_key):
+    # use random key
+    result = Base62.encodebytes(hashlib.md5(
+        str(time.time()).encode('utf-8')).digest()[-5:])
+    if result != url_key:
+        return result
+    else:
+        return use_rand_key(url_key)
+
+
+class Create_short_url(BaseModel):
+    original_url: str
+
+
+@app.post("/create")
+def shorten_request(data: Create_short_url):
+    """
+    Get the shorten request from client.
+    """
+    if urlparse(data.original_url).scheme == "":
+        raise HTTPException(
+            status_code=400, detail="URL should have a scheme")
+    url = data.original_url
+    url_key = short(url)
+    timestamp = time.time()
+    timestring = time.strftime("%Y/%m/%d %H:%M:%S", time.localtime())
+    if DB.get(url_key) == None:
+        DB.put(
+            {
+                "original_url": data.original_url,
+                "timestamp": timestamp,
+                "timestring": timestring,
+                "create_method": "hash",
+                "used_count": 0
+            }, url_key
+        )
+        return {"url": SERVER_PREFIX + url_key}
+    elif DB.get(url_key) != None and DB.get(url_key)["original_url"] != data.original_url:
+        url_key = use_rand_key(url_key)
+        DB.put(
+            {
+                "original_url": data.original_url,
+                "timestamp": timestamp,
+                "timestring": timestring,
+                "create_method": "rand",
+                "used_count": 0
+            }, url_key
+        )
+        return {"url": SERVER_PREFIX + url_key}
+    elif DB.get(url_key) != None and DB.get(url_key)["original_url"] == data.original_url:
+        return {"url": SERVER_PREFIX + url_key}
+
+
+@app.get("/{url_key}")
+def redirect_to_url(url_key):
+    """
+    Check the url_key is in DB, redirect to original url.
+    """
+    data = DB.get(url_key)
+    if data is None:
+        raise HTTPException(status_code=404, detail="Key not found")
+    else:
+        used_count = data["used_count"] + 1
+        original_url = data["original_url"]
+        DB.put(
+            {
+                "original_url": data["original_url"],
+                "timestamp": data["timestamp"],
+                "timestring": data["timestring"],
+                "create_method": data["create_method"],
+                "used_count": used_count
+            }, url_key
+        )
+        return RedirectResponse(original_url)
